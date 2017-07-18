b52662aa4c24c0b7d5a2812e70ef784695aa290f
Gordon RASS-KWASI <grass-kw@e2r2p8.42.fr>
2017-07-18 14:34:28 +0200

    Roll & ball

---
+++ Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private Rigidbody rb;
+	public Text CountText;
+	public Text WinText;
+	public float Speed;
+	private int count;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCount ();
+		WinText.text = "";
+	}
+
+	void FixedUpdate() {
+		float MoveHorizontal = -Input.GetAxis("Horizontal");
+		float MoveVertical = -Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (MoveHorizontal, 0.0f, MoveVertical);
+		rb.AddForce(movement * Speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setCount ();
+		}
+	}
+
+	void setCount() {
+		CountText.text = "count : " + count.ToString ();
+		if (count == 13)
+			WinText.text = "Win !";
+	}
+}
